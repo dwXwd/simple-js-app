e5f81896efbd6f2c588d3181b9c6832dd1f04e2c
dwXwd <dennisweissert.wd@gmail.com>
2022-01-15 16:11:12 +0100

    Deleted Old Entries

--- js/scripts.js
+++ js/scripts.js
@@ -1,9 +1,2 @@
-alert('Hello World');
-let favoriteFood= ('Sushi');
-document.write(favoriteFood);
 
-let size = 577;
-let doubleSize = size * 2;
-let minSize = doubleSize * 2 - size;
 
-document.write(minSize);
