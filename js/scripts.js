096397f0c003156ba545dd468c3995cb034cf8f9
dwXwd <dennisweissert.wd@gmail.com>
2022-01-23 11:48:11 +0100

    Put pokemonList and pokemonList2 together

--- js/scripts.js
+++ js/scripts.js
@@ -1,29 +1,59 @@
 //created array for distinct Pokemon-Objects.
 
+  let pokemonList = [
+    {
+    name:'Bulbasaur',
+    types: ['Plant', 'Poison'],
+    height: 0.7,
+    evolution:'1 of 3',
+    generation: 1
+    },
 
-let pokemonList = [
-  {
-  name:'Bulbasaur',
-  types: ['Plant', 'Poison'],
-  height: 0.7,
-  evolution:'1 of 3',
-  generation: 1
-  },
+    {
+    name:'Ho-oh',
+    types: ['Fire', 'Flying'],
+    height: 3.8,
+    evolution:'1 of 1',
+    generation: 2
+    },
 
-  {
-  name:'Ho-oh',
-  types: ['Fire', 'Flying'],
-  height: 3.8,
-  evolution:'1 of 1',
-  generation: 2
-  },
+    {
+    name:'Archeops',
+    types: ['Stone', 'Flying'],
+    height: 1.4,
+    evolution:'2 of 2',
+    generation: 5
+    },
 
-  {
-  name:'Archeops',
-  types: ['Stone', 'Flying'],
-  height: 1.4,
-  evolution:'2 of 2',
-  generation: 5
+    {
+    name:'Parasect',
+    types: ['Bug', 'Plant'],
+    height: 0.7,
+    evolution:'2 of 2',
+    generation: 1
+    },
+
+    {
+    name:'Lugia',
+    types: ['Psychic', 'Flying'],
+    height: 3.8,
+    evolution:'1 of 1',
+    generation: 2
+    },
+
+    {
+    name:'Archeops',
+    types: ['Stone', 'Flying'],
+    height: 1.4,
+    evolution:'2 of 2',
+    generation: 5
+    }
+  ];
+
+  function add(pokemon) {
+    pokemonList.push(pokemon);
+  }
+
   }
 ]
 
