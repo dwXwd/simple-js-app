7cdd2625d8a948e168ad4c9cbae2f8ddc27920dc
dwXwd <dennisweissert.wd@gmail.com>
2022-01-28 20:07:52 +0100

    Emptied pokemonList -array

--- js/scripts.js
+++ js/scripts.js
@@ -1,55 +1,6 @@
 //created array for distinct Pokemon-Objects.
 
 let pokemonRepository = (function() {
-  let pokemonList = [
-    {
-    name:'Bulbasaur',
-    types: ['Plant', 'Poison'],
-    height: 0.7,
-    evolution:'1 of 3',
-    generation: 1
-    },
-
-    {
-    name:'Ho-oh',
-    types: ['Fire', 'Flying'],
-    height: 3.8,
-    evolution:'1 of 1',
-    generation: 2
-    },
-
-    {
-    name:'Archeops',
-    types: ['Stone', 'Flying'],
-    height: 1.4,
-    evolution:'2 of 2',
-    generation: 5
-    },
-
-    {
-    name:'Parasect',
-    types: ['Bug', 'Plant'],
-    height: 0.7,
-    evolution:'2 of 2',
-    generation: 1
-    },
-
-    {
-    name:'Lugia',
-    types: ['Psychic', 'Flying'],
-    height: 3.8,
-    evolution:'1 of 1',
-    generation: 2
-    },
-
-    {
-    name:'Onix',
-    types: ['Stone', 'Ground'],
-    height: 1.4,
-    evolution:'1 of 2',
-    generation: 1
-    }
-  ];
 
   function add(pokemon) {
     if(typeof pokemon === 'object'){
